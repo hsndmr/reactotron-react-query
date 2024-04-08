e4059dde325c4b923293435cbfed8af35fa5051a
demirjs <41634504+hsndmr@users.noreply.github.com>
2024-04-08 21:11:59 +0300

    Add React import and fix formatting in App.tsx

--- example/App.tsx
+++ example/App.tsx
@@ -2,9 +2,10 @@ import { StyleSheet, View } from 'react-native';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './queryClient';
 import PokemonList from './PokemonList';
+import React from 'react';
 
-if(__DEV__) {
-  require("./reactotron.ts")
+if (__DEV__) {
+  require('./reactotron.ts');
 }
 
 export default function App() {
